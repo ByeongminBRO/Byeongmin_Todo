1016a9c13a8c550ef5791d653aa03dba5a94d5ca
ByeongminBRO <abm40172138@gmail.com>
2023-02-09 16:26:32 +0900

    todo완성

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import "./App.css";
+import Template from "./components/Template";
+import TodoList from "./components/TodoList";
+import { MdAddCircle } from "react-icons/md";
+import TodoInsert from "./components/TodoInsert";
+
+
+
+let nextId = 0;
+
+const App = () => {
+  const [selectedTodo, setSelectedTodo] = useState(null);
+  const [insertToggle, setInsertToggle] = useState(false);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "코딩공부",
+      checked: false
+    },
+
+    {
+      id: 2,
+      text: "알고리즘 문제풀이",
+      checked: false
+    },
+
+    {
+      id: 3,
+      text: "운동",
+      checked: false
+    },
+
+    {
+      id: 4,
+      text: "추가",
+      checked: false
+    }
+
+  ]);
+
+  const onInsertToggle = () => {
+    if (selectedTodo) {
+      setSelectedTodo(null);
+    }
+    setInsertToggle(prev => !prev);
+  };
+
+  const onInsertTodo = text => {
+    if (text === "") {
+      
+    } else {
+      const todo = {
+        id: nextId,
+        text,
+        checked: false
+      };
+      setTodos(todos => todos.concat(todo));
+      nextId++;
+    }
+  };
+
+  const onCheckToggle = id => {
+    setTodos(todos =>
+      todos.map(todo =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
+
+  const onChangeSelectedTodo = todo => {
+    setSelectedTodo(todo);
+  };
+
+  const onRemove = id => {
+    onInsertToggle();
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  };
+
+  const onUpdate = (id, text) => {
+    onInsertToggle();
+    setTodos(todos =>
+      todos.map(todo => (todo.id === id ? { ...todo, text } : todo))
+    );
+  };
+
+  return (
+    <Template todoLength={todos.length}>
+      <TodoList
+        todos={todos}
+        onCheckToggle={onCheckToggle}
+        onInsertToggle={onInsertToggle}
+        onChangeSelectedTodo={onChangeSelectedTodo}
+      />
+      <div className="add-todo-button" onClick={onInsertToggle}>
+        <MdAddCircle />
+      </div>
+      {insertToggle && (
+        <TodoInsert
+          selectedTodo={selectedTodo}
+          onInsertToggle={onInsertToggle}
+          onInsertTodo={onInsertTodo}
+          onRemove={onRemove}
+          onUpdate={onUpdate}
+        />
+      )}
+    </Template>
+  );
+};
+
+export default App;
